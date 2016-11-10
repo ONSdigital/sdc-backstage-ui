2eb7ecf9b024697220e178e47a3116e67b402a28
Shane Edwards <shane.edwards9@gmail.com>
2016-11-10 09:43:35 +0000

    Fix for missing state property assignment for collection exercise details.

--- app/reducers/ui.jsx
+++ app/reducers/ui.jsx
@@ -35,7 +35,7 @@ var uiReducer = function (state, action) {
 			newState.collectionExercise.list.activeFilter = action.status;
 			return newState;
 		case 'REQUEST_COLLECTION_EXERCISE_DETAILS':
-			newState.collectionExercise.details = action.collectionExercise.id;
+			newState.collectionExercise.details.id = action.collectionExercise.id;
 			return newState;
 		case 'UI_COLLECTION_EXERCISE_DETAILS':
 			newState.collectionExercise.details.id = action.id;
